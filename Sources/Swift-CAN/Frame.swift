7b3e7d7a8afe065b93a75cfb230aaf31b52de4a2
Dr. Michael Lauer <mickey@vanille-media.de>
2021-10-04 15:45:15 +0200

    Frame: allow settings timestamp at construction time

--- Sources/Swift-CAN/Frame.swift
+++ Sources/Swift-CAN/Frame.swift
@@ -15,21 +15,21 @@ public extension CAN {
         public let timestamp: Double
 
         /// Create a padded CAN frame from data. DLC is hardcoded to 8.
-        public init(id: UInt32, padded data: [UInt8], pad: UInt8 = 0xAA) {
+        public init(id: UInt32, padded data: [UInt8], pad: UInt8 = 0xAA, timestamp: Double = 0) {
             self.id = id
             var data = data
             while data.count < 8 { data.append(pad) }
             self.dlc = data.count
             self.data = data
-            self.timestamp = 0
+            self.timestamp = timestamp
         }
 
         /// Create an unpadded CAN frame from data. DLC is taken from length of data.
-        public init(id: UInt32, unpadded data: [UInt8]) {
+        public init(id: UInt32, unpadded data: [UInt8], timestamp: Double = 0) {
             self.id = id
             self.dlc = data.count
             self.data = data
-            self.timestamp = 0
+            self.timestamp = timestamp
         }
     }
 }
