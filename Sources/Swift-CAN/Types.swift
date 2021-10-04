cfe96ab2803791d8bcfa9d8dd6b1cb2756254230
Dr. Michael Lauer <mickey@vanille-media.de>
2021-10-04 21:35:59 +0200

    Types: add broadcast arbitration IDs

--- Sources/Swift-CAN/Types.swift
+++ Sources/Swift-CAN/Types.swift
@@ -3,4 +3,8 @@
 public extension CAN {
 
     typealias ArbitrationId = UInt32
+
+    /// Functional adresses when talking to all ECUs
+    static let BroadcastArbitrationId11: ArbitrationId = .init(0x7DF)
+    static let BroadcastArbitrationId29: ArbitrationId = .init(0x18DB33F1)
 }
