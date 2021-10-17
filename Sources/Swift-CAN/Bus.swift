b3950953ef9fcfa4a6ca1adc6be0ff5127f238fa
Dr. Michael Lauer <mickey@vanille-media.de>
2021-10-17 15:47:04 +0200

    Add protocol for an interface where CAN frames can be sent to and received from

--- Sources/Swift-CAN/Bus.swift
+++
@@ -1,2 +0,0 @@
-// This file is part of Swift-CAN. (C) Dr. Michael 'Mickey' Lauer <mickey@vanille-media.de>
-
---
+++ Sources/Swift-CAN/Interface.swift
@@ -0,0 +1,9 @@
+// This file is part of Swift-CAN. (C) Dr. Michael 'Mickey' Lauer <mickey@vanille-media.de>
+
+public protocol _CANInterface {
+
+    func open() throws
+    func close() throws
+    func read(timeout: Double) throws -> CAN.Frame
+    func write(_ frame: CAN.Frame) throws
+}
