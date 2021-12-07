b11cd945f8dca4c4a7dcb0801f5f8cc13f72792e
Dr. Michael Lauer <mickey@vanille-media.de>
2021-12-07 11:21:43 +0100

    merge

--- Sources/Swift-CAN/Interface.swift
+++ Sources/Swift-CAN/Interface.swift
@@ -1,10 +1,30 @@
 // This file is part of Swift-CAN. (C) Dr. Michael 'Mickey' Lauer <mickey@vanille-media.de>
 
+<<<<<<< HEAD
 /// A CAN interface.
 public protocol _CANInterface {
 
     /// Open the interface with the chosen `bitrate`.
     func open(baudrate: Int) throws
+=======
+/// A CANbus interface.
+public protocol _CANInterface {
+
+    /// The vendor name.
+    var vendor: String { get }
+    /// The model name.
+    var model: String { get }
+    /// The serial number.
+    var serialNumber: String { get }
+
+    /// The current bitrate.
+    var bitrate: Int { get }
+    /// The interface name.
+    var name: String { get }
+
+    /// Open the interface with the chosen `bitrate`.
+    func open(bitrate: Int) throws
+>>>>>>> Interface: add a way to get metadata for the CAN interface
     /// Close the interface.
     func close() throws
     /// Read a CAN frame with a `timeout` in milliseconds.
