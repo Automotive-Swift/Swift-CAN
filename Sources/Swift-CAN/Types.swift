8efc0da6f0fd9aa0e9fdcd09d6147958bdbfabb9
Dr. Michael Lauer <mickey@vanille-media.de>
2021-12-07 16:21:35 +0100

    Types: refactor the CAN broadcast IDs

--- Sources/Swift-CAN/Types.swift
+++ Sources/Swift-CAN/Types.swift
@@ -4,8 +4,18 @@ public extension CAN {
 
     /// The CAN adressing unit. Allowed are 11-bit or 29-bit addresses.
     typealias ArbitrationId = UInt32
+}
+
+public extension CAN.ArbitrationId {
 
     /// Functional CAN adresses when talking to all ECUs.
-    static let BroadcastArbitrationId11: ArbitrationId = .init(0x7DF)
-    static let BroadcastArbitrationId29: ArbitrationId = .init(0x18DB33F1)
+    static let Broadcast11: Self = 0x7DF
+    static let Broadcast29: Self = 0x18DB33F1
+}
+
+public extension CAN.ArbitrationId {
+
+    /// Returns `true`, if the sender is a CAN broadcast address.
+    var isBroadcast: Bool { self == Self.Broadcast11 || self == Self.Broadcast29 }
+
 }
